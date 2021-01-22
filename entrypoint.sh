85c2f7fcdeee7605eb2d53cf4a0ce6b68286f8f1
Christopher - RtF <58520035+christopher-rtf@users.noreply.github.com>
2021-01-22 14:32:43 -0400

    Complete simplification of env substitution scripting

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,4 @@ cat $WWWDIR/settings.js
 envsubst '$MORPHIC_WINDOWS_APPCAST_URL:$MORPHIC_WINDOWS_COMMUNITY_APPCAST_URL:$MORPHIC_MAC_APPCAST_URL' < /MorphicLiteWeb/config/nginx.conf > /MorphicLiteWeb/config/nginx.conf.subst
 mv /MorphicLiteWeb/config/nginx.conf.subst /MorphicLiteWeb/config/nginx.conf
 
-# envsubst '$MORPHIC_WINDOWS_APPCAST_URL' < /MorphicLiteWeb/config/nginx.conf > /MorphicLiteWeb/config/nginx.conf.subst
-# envsubst '$MORPHIC_WINDOWS_COMMUNITY_APPCAST_URL' < /MorphicLiteWeb/config/nginx.conf.subst > /MorphicLiteWeb/config/nginx.conf
-# envsubst '$MORPHIC_MAC_APPCAST_URL' < /MorphicLiteWeb/config/nginx.conf > /MorphicLiteWeb/config/nginx.conf.subst
-# mv /MorphicLiteWeb/config/nginx.conf.subst /MorphicLiteWeb/config/nginx.conf
-
 nginx -p /MorphicLiteWeb -c config/nginx.conf -g "daemon off;"
